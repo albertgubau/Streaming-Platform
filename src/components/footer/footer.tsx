fccc4d0fd5b8781b3a918489845610652bd0032e
albertgubau <agubau11@gmail.com>
2025-05-12 17:28:24 +0200

    chore: add footer lowercase

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {FooterColumn, FooterLinks, FooterStyled, FooterWrapper, StyledText} from './footer.styles'
+import Logo from '../../assets/rakuten-logo.svg'
+
+export default function Footer() {
+    return (
+        <FooterStyled>
+            <FooterWrapper>
+                <FooterLinks>
+                    <FooterColumn>
+                        <span>About Us</span>
+                        <span>Services</span>
+                        <span>Contact</span>
+                        <span>Privacy Policy</span>
+                    </FooterColumn>
+                    <FooterColumn>
+                        <span>About Us</span>
+                        <span>Services</span>
+                        <span>Contact</span>
+                        <span>Privacy Policy</span>
+                    </FooterColumn>
+                    <FooterColumn>
+                        <span>About Us</span>
+                        <span>Services</span>
+                        <span>Contact</span>
+                        <span>Privacy Policy</span>
+                    </FooterColumn>
+                </FooterLinks>
+                <Logo className="footer-logo" />
+            </FooterWrapper>
+
+            <StyledText>© 2024 Albert Gubau. All rights reserved.</StyledText>
+        </FooterStyled>
+    )
+}
